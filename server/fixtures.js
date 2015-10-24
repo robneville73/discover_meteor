0dd1d34f83fa32a346dd98ddb514048d3347103e
Rob Neville <robertneville73@gmail.com>
2015-10-24 10:01:24 -0400

    Denormalized the number of comments into the post

--- server/fixtures.js
+++ server/fixtures.js
@@ -18,7 +18,8 @@ if (Posts.find().count() === 0) {
     url: 'http://sachagreif.com/introducing-telescope/',
     userId: sacha._id,
     author: sacha.profile.name,
-    submitted: new Date(now - 7 * 3600 * 1000)
+    submitted: new Date(now - 7 * 3600 * 1000),
+    commentsCount: 2
   });
 
   Comments.insert({
@@ -42,7 +43,8 @@ if (Posts.find().count() === 0) {
     url: 'http://meteor.com',
     userId: tom._id,
     author: tom.profile.name,
-    submitted: new Date(now - 10 * 3600 * 1000)
+    submitted: new Date(now - 10 * 3600 * 1000),
+    commentsCount: 0
   });
 
   Posts.insert({
@@ -50,6 +52,7 @@ if (Posts.find().count() === 0) {
     url: 'http://themeteorbook.com',
     userId: tom._id,
     author: tom.profile.name,
-    submitted: new Date(now - 12 * 3600 * 1000)
+    submitted: new Date(now - 12 * 3600 * 1000),
+    commentsCount: 0
   });
 }
